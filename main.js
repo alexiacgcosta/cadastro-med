57896c71342173524003a43425ce99dbdd7502df
alexiacgcosta <alexia.cassia09@gmail.com>
2023-04-06 09:49:34 -0400

    ajusting the first page

--- main.js
+++ main.js
@@ -1,27 +1,37 @@
-var meds = {};
+// seleciona o formulário HTML
+const form = document.querySelector('#my-form');
 
-function addMed() {
-    event.preventDefault();
-    var newMed = {
-        "medName": document.getElementById("medName").value,
-        "medQtd": document.getElementById("medQnt").value,
-        "medPrice": document.getElementById("medPrice").value,
-    }
-    resetForm();
-    meds.push(newMed);
-    renderDiv();
-}
-function renderDiv(){
-  let div = document.getElementById("container-new-med");
-  div.innerHTML =  `<p>${newMed.medName}</p>
-  <p>${newMed.medQtd} unidades disponíveis</p>
-  <p>${newMed.medPrice} Reais por Unidade</p>;`
+// adiciona um ouvinte de evento para o envio do formulário
+form.addEventListener('submit', function(e) {
+  e.preventDefault(); // evita que a página seja recarregada
+
+  // captura os valores dos campos do formulário
+  const name = form.elements['name'].value;
+  const qnt = form.elements['qnt'].value;
+  const price = form.elements['price'].value;
+  const classe = form.elements['classe'].value;
+
+  // cria um objeto com os valores capturados
+  const formData = { name, qnt, price, classe };
+  console.log(formData);
+
+  // armazena o objeto em localStorage para acessá-lo na nova página
+  localStorage.setItem('formData', JSON.stringify(formData));
+
+  // redireciona para a nova página
+  window.location.href = 'sucess.html';
+});
+
+// função para exibir o resumo dos dados na nova página
+function showSummary() {
+  // recupera os dados armazenados em localStorage
+  const formData = JSON.parse(localStorage.getItem('formData'));
+
+  // exibe os dados na nova página
+  document.querySelector('#name-summary').textContent = `Nome: ${formData.name}`;
+  document.querySelector('#qnt-summary').textContent = ` ${formData.qnt} unidades disponíveis`;
+  document.querySelector('#price-summary').textContent = `${formData.price} reais por unidade`;
 }
 
-function resetForm(){
-  document.getElementById("medName").value = "";
-  document.getElementById("medQnt").value = "";
-  document.getElementById("medPrice").value = "";
-}
-
-renderDiv();
+// chama a função showSummary quando a nova página é carregada
+window.onload = showSummary;
